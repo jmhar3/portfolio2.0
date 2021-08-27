9c9527bb1a0bd2e55adcc15ad04f0e7bcdbec207
jmhar <jmhar@protonmail.com>
2021-08-27 14:43:35 +1000

    basic blog content

--- src/containers/blog.js
+++ src/containers/blog.js
@@ -1,6 +1,13 @@
+import notebook from '../images/notebook.png'
+
 const Blog = () => {
     return (
-        <></>
+        <section id="blog">
+            <div id="blog-header">
+                <a href="https://medium.com/@jmhar" target="_blank"><img src={notebook} /></a>
+                <h3>Blog</h3>
+            </div>
+        </section>
     )
 }
 
