cb52a46c310ab8d1d99e799a33f4c38be5c5a5c2
jmhar <jmhar@protonmail.com>
2021-08-27 14:31:18 +1000

    typography

---
+++ src/containers/work.js
@@ -0,0 +1,17 @@
+const Work = () => {
+    return (
+        <section id="work">
+            <h3>Work</h3>
+            <p>I built this website using the skills I've learnt over the past month. You can see some of my other
+                creations below.</p>
+            <a href="https://jmhar3.github.io/pokedex/index.html" target="_blank"><img src="./images/pokedexsite.jpg"
+                class="work" /></a>
+            <a href="https://jmhar3.github.io/glasshouse" target="_blank"><img src="./images/creations page.jpg"
+                class="work" /></a>
+            <a href="https://jmhar3.github.io/glasshouse" target="_blank"><img src="./images/palette page.jpg"
+                class="work" /></a>
+        </section>
+    )
+}
+
+export default Work;
