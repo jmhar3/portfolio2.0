ac1a0f7844bc129089b1712acd5dfcd1e3214470
jmhar <jmhar@protonmail.com>
2022-02-04 20:51:54 +1100

    add text + marquee animations

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,35 +1,41 @@
 import globe from '../images/globe.png';
 import mainImage from '../images/retro.png';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
-    return (
-        <section id="about">
-            <div className="column about-container">
-                <div className="strip about-top"></div>
-                <div className="row">
-                    <div className="strip about-left"></div>
-                    <div class="about-info">
-                        <div id="name">
-                            <h2 id="about-label">Software Engineer</h2>
-                            <h1>Jessica Harriman</h1>
-                        </div>
-                        <div id="location">
-                                <img src={globe} />
-                                <h4>Melbourne, Australia</h4>
-                        </div>
+ return (
+  <section id="about">
+   <div className="column about-container">
+    <div className="strip about-top"></div>
+    <div className="row">
+     <div className="strip about-left"></div>
+     <div class="about-info">
+      <div id="name">
+       <h2 id="about-label">Software Engineer</h2>
+      <h1><Typewriter
+        onInit={(typewriter) => {
+         typewriter.typeString("Hi, my name's Jessica Harriman")
+          .start();
+        }}
+       /></h1>
+      </div>
+      <div id="location">
+       <img src={globe} />
+       <h4>Melbourne, Australia</h4>
+      </div>
 
-                        <p className="about">Welcome to my portfolio. I'm an aspiring software developer and UI designer, currently studying at Academy xi. When I'm not hard at work I'm cuddling with my dog, playing video games or (more often than not) a bit of both.</p>
+      <p className="about">Welcome to my portfolio. I'm a Software Developer and UI designer currently working at HealthAide. When I'm not hard at work I'm cuddling with my dog, playing video games or (more often than not) a bit of both. In my off time I enjoy building and creating in many ways, from Lego and puzzles to coding my own fun little projects.</p>
 
-                        <p className="tech">HTML   •   CSS    •   JavaScript   •   React   •   Redux   •   Ruby   •   Rails   •   Firebase   •   GitHub   •   Chrome DevTools</p>
-                    </div>
-                </div>
-                <div className="strip about-bottom"></div>
-            </div>
-            <div class="image-container">
-                <img src={mainImage} id="main-image" />
-            </div>
-        </section>
-    )
+      <p className="tech">HTML   •   CSS    •   JavaScript   •   React   •   Redux   •   Ruby   •   Rails   •   Firebase   •   GitHub   •   Chrome DevTools</p>
+     </div>
+    </div>
+    <div className="strip about-bottom"></div>
+   </div>
+   <div class="image-container">
+    <img src={mainImage} id="main-image" />
+   </div>
+  </section>
+ )
 }
 
 export default Home;
--- src/containers/work_links.js
+++ src/containers/work_links.js
@@ -1,23 +1,31 @@
-import '../styling/work-links.css';import { Link } from "react-router-dom";
+import '../styling/work-links.css';
+import { Link } from "react-router-dom";
+import Marquee from "react-fast-marquee";
 
 const WorkLinks = () => {
 
-    return (
-        <container id="work-links">
-            <Link to="/pokedex">Pokedex</Link>
-            <Link to="/glasshouse">Glasshouse</Link>
-            <Link to="/apocalypto">Apocalypto</Link>
-            <Link to="/dreamjournal">DreamJournal</Link>
-            <Link to="/pokedex">Pokedex</Link>
-            <Link to="/glasshouse">Glasshouse</Link>
-            <Link to="/apocalypto">Apocalypto</Link>
-            <Link to="/dreamjournal">DreamJournal</Link>
-            <Link to="/pokedex">Pokedex</Link>
-            <Link to="/glasshouse">Glasshouse</Link>
-            <Link to="/apocalypto">Apocalypto</Link>
-            <Link to="/dreamjournal">DreamJournal</Link>
-        </container>
-    )
+ return (
+  <container id="work-links">
+   <Marquee gradient={false}>
+    <Link to="/pokedex">Pokedex</Link>
+    <Link to="/glasshouse">Glasshouse</Link>
+    <Link to="/apocalypto">Apocalypto</Link>
+    <Link to="/dreamjournal">DreamJournal</Link>
+    <Link to="/pokedex">Pokedex</Link>
+    <Link to="/glasshouse">Glasshouse</Link>
+    <Link to="/apocalypto">Apocalypto</Link>
+    <Link to="/dreamjournal">DreamJournal</Link>
+    <Link to="/pokedex">Pokedex</Link>
+    <Link to="/glasshouse">Glasshouse</Link>
+    <Link to="/apocalypto">Apocalypto</Link>
+    <Link to="/dreamjournal">DreamJournal</Link>
+    <Link to="/pokedex">Pokedex</Link>
+    <Link to="/glasshouse">Glasshouse</Link>
+    <Link to="/apocalypto">Apocalypto</Link>
+    <Link to="/dreamjournal">DreamJournal</Link>
+   </Marquee>
+  </container>
+ )
 }
 
 export default WorkLinks;
