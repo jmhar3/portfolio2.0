9c23b5c4f9764252b80f6e3363c58fec9798ff60
jmhar <jmhar@protonmail.com>
2022-02-05 11:27:24 +1100

    top nav buttons smoooth scroll to top/contact

--- src/containers/nav.js
+++ src/containers/nav.js
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom";
 import '../styling/nav.css';
 
 const Nav = () => {
-    return (
-        <nav>
-            <Link to="/" class="home-button">JH</Link>
-            <Link to="/" class="contact-button">GET IN TOUCH</Link>
-        </nav>
-    )
+ function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+ }
+
+ function scrollToContact() {
+  document.getElementById('contact').scrollIntoView()
+ }
+
+ return (
+  <nav>
+   <button
+    onClick={scrollToTop}
+    class="home-button"
+   >JH</button>
+   <button
+    onClick={scrollToContact}
+    class="contact-button"
+   >GET IN TOUCH</button>
+  </nav>
+ )
 }
 
 export default Nav;
