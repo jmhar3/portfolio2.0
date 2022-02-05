d7a9166b4a0e8a933672d0d5b25f118fd486f641
jmhar <jmhar@protonmail.com>
2022-02-05 12:58:02 +1100

    deploy website

--- src/containers/contact.js
+++ src/containers/contact.js
@@ -1,28 +1,28 @@
 const Contact = () => {
-    return (
-        <section id="contact">
-            <div className="row contact-title">
-                <h3>Get In Touch</h3>
-                <div className="strip contact-strip"></div>
-            </div>
-            <span className="contact-info row">
-                <h4>0499833390</h4>
-                •<a href="mailto:jmhar@protonmail.com">
-                jmhar @protonmail.com
-                </a>
-            </span>
+ return (
+  <section id="contact">
+   <div className="row contact-title">
+    <h3>Get In Touch</h3>
+    <div className="strip contact-strip"></div>
+   </div>
+   <span className="contact-info row">
+    <h4>0499833390</h4>
+    •<a href="mailto:jmhar@protonmail.com">
+     jmhar@protonmail.com
+    </a>
+   </span>
 
-            <form id="contact_form" action="https://formkeep.com/f/8395fde77958"
-                accept-charset="UTF-8"
-                enctype="multipart/form-data"
-                method="POST">
-                <input type ="text" id="Name" placeholder="Name" required />
-                <input type ="email" id="Email" placeholder="Email" required />
-                <textarea id="Message" placeholder="Message" rows="6" maxlength="3000" required />
-                <button type ="submit" id="submit">Send Message</button>
-            </form>
-        </section>
-    )
+   <form id="contact_form" action="https://formkeep.com/f/8395fde77958"
+    accept-charset="UTF-8"
+    enctype="multipart/form-data"
+    method="POST">
+    <input type="text" id="Name" placeholder="Name" required />
+    <input type="email" id="Email" placeholder="Email" required />
+    <textarea id="Message" placeholder="Message" rows="6" maxlength="3000" required />
+    <button type="submit" id="submit" style={{width: 'fit-content'}}>Send Message</button>
+   </form>
+  </section>
+ )
 }
 
 export default Contact;
